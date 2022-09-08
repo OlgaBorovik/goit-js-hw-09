3dac7c5f8f52a06fd97089d0692f0a075ea040bb
OlgaBorovik <99828278+OlgaBorovik@users.noreply.github.com>
2022-09-08 14:13:55 +0000

    Deploying to gh-pages from @ OlgaBorovik/goit-js-hw-09@0104d8fe7b9bebda57fd6a78ebaad18c5988b0c1 🚀

--- 03-promises.3d0dffa6.js
+++
@@ -1,2 +0,0 @@
-!function(){var e,t;({form:document.querySelector(".form"),delay:document.querySelector('input[name="delay"]'),step:document.querySelector('input[name="step"]'),amount:document.querySelector('input[name="amount"]'),btn:document.querySelector("button")}).form.addEventListener("submit",new Promise((function(n){Math.random()>.3&&n({position:e,delay:t})})))}();
-//# sourceMappingURL=03-promises.3d0dffa6.js.map
--- 03-promises.b3809efd.js
+++
@@ -1,2 +0,0 @@
-var e,t;({form:document.querySelector(".form"),delay:document.querySelector('input[name="delay"]'),step:document.querySelector('input[name="step"]'),amount:document.querySelector('input[name="amount"]'),btn:document.querySelector("button")}).form.addEventListener("submit",new Promise((n=>{Math.random()>.3&&n({position:e,delay:t})})));
-//# sourceMappingURL=03-promises.b3809efd.js.map
